9243b07edae9542f495a0d2ed19dea4514a45901
mikeyuchima <tomichaels@gmail.com>
2020-03-11 14:50:29 -0400

    added boilerplate

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+
+const Header = props => {
+  <div>
+    <div className="bar">
+      <a href="">sick fits</a>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div>Cart</div>
+  </div>;
+};
