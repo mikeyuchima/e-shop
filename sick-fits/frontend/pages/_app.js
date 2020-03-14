fadc1babbfb263dc9b4c75be4e51549c20e92bf3
mikeyuchima <tomichaels@gmail.com>
2020-03-14 15:47:42 -0400

    init apollo and item cmp

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,16 +1,37 @@
+//library
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+
+//wrapper
+import withData from '../lib/withData';
+
+//components
 import Page from '../components/Page';
 
-export default class MyApp extends App {
+class MyApp extends App {
+  static async getInitialProps({ Component, context }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(context);
+    }
+    //this exposes query to user
+    pageProps.query = context.query;
+    return { pageProps };
+  }
+
   render() {
     const { Component } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
+
+export default withData(MyApp);
